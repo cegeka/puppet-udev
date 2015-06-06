jhoblitt/puppet-module_skel

update spec_helper_acceptance to use #puppet_module_install
Instead of custom scp logic

spec/spec_helper_acceptance.rb
@@ -27,16 +27,8 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
+    puppet_module_install(:source => proj_root, :module_name => 'module_skel')
     hosts.each do |host|
-      on host, "mkdir -p #{host['distmoduledir']}/module_skel"
-      result = on host, "echo #{host['distmoduledir']}/module_skel"
-      target = result.raw_output.chomp
-
-      %w(files lib manifests templates metadata.json).each do |file|
-        path = "#{proj_root}/#{file}"
-        next unless File.exists? path
-        scp_to host, path, target
-      end
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), {:acceptable_exit_codes => [0, 1]}
       on host, puppet('module', 'install', 'stahnma-epel'), {:acceptable_exit_codes => [0, 1]}
     end

cegeka/puppet-udev

spec/spec_helper_acceptance.rb
@@ -27,16 +27,8 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
+    puppet_module_install(:source => proj_root, :module_name => 'udev')
     hosts.each do |host|
-      on host, "mkdir -p #{host['distmoduledir']}/udev"
-      result = on host, "echo #{host['distmoduledir']}/udev"
-      target = result.raw_output.chomp
-
-      %w(files lib manifests templates metadata.json).each do |file|
-        path = "#{proj_root}/#{file}"
-        next unless File.exists? path
-        scp_to host, path, target
-      end
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), {:acceptable_exit_codes => [0, 1]}
       on host, puppet('module', 'install', 'stahnma-epel'), {:acceptable_exit_codes => [0, 1]}
     end