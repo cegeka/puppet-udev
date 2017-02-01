4792eaa3e68e757ce1a1d31f9df8ea9eb7544362
Fabian Dammekens <fabian.dammekens@cegeka.be>
2017-02-01 10:17:25 +0100

    [RSPEC] Don't rely on custom functions

--- spec/acceptance/udev_spec.rb
+++ spec/acceptance/udev_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper_acceptance'
 
 describe 'udev class' do
-  maj = fact_on 'master', 'operatingsystemmajrelease'
-
-  package_name = nil
-  case maj.to_i
-  when 5, 6
-    package_name = 'udev'
-  when 7
-    package_name = 'systemd'
-  end
 
   describe 'running puppet code' do
     it 'should work with no errors' do
@@ -31,7 +22,7 @@ describe 'udev class' do
     end
   end
 
-  describe package(package_name) do
+  describe package('udev') do
     it { should be_installed }
   end
 
